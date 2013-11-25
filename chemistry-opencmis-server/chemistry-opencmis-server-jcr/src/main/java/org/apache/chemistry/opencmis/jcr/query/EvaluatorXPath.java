d18b33ff66f71c31c7ac12684e9c18c03f593d45
nick.knysh <nick.knysh@engagepoint.com>
2013-11-25 15:06:50 +0200

    EDOC-?? : case insensitive <like> and <not like>

--- chemistry-opencmis-server/chemistry-opencmis-server-jcr/src/main/java/org/apache/chemistry/opencmis/jcr/query/EvaluatorXPath.java
+++ chemistry-opencmis-server/chemistry-opencmis-server-jcr/src/main/java/org/apache/chemistry/opencmis/jcr/query/EvaluatorXPath.java
@@ -191,17 +191,47 @@ public class EvaluatorXPath extends EvaluatorBase<XPathBuilder> {
 
     @Override
     public XPathBuilder like(XPathBuilder op1, XPathBuilder op2) {
-        return new FunctionBuilder("jcr:like", op1, op2);
+        XPathBuilder op11 = op1;
+        XPathBuilder op21 = op2;
+        if (op2 != null && op2 instanceof LiteralBuilder) {
+            op11 = new FunctionBuilder("UPPER-CASE", op1);
+            op21 = new LiteralBuilderForLikeWrapper(((LiteralBuilder) op2));
+        }
+        return new FunctionBuilder("jcr:like", op11, op21);
     }
 
     @Override
     public XPathBuilder notLike(XPathBuilder op1, XPathBuilder op2) {
-        return new FunctionBuilder("jcr:like", op1, op2) {
+        XPathBuilder op11 = op1;
+        XPathBuilder op21 = op2;
+        if (op2 != null && op2 instanceof LiteralBuilder) {
+            op11 = new FunctionBuilder("UPPER-CASE", op1);
+            op21 = new LiteralBuilderForLikeWrapper(((LiteralBuilder) op2));
+        }
+        return new FunctionBuilder("jcr:like", op11, op21) {
             @Override
             public String xPath() {
                 return "not(" + super.xPath() + ")"; 
             }
         };
+    }
+
+    // wrapper that makes text upper case for LIKE operation
+    private static class LiteralBuilderForLikeWrapper extends PrimitiveBuilder  {
+
+        private LiteralBuilder delegate;
+
+        public LiteralBuilderForLikeWrapper(LiteralBuilder value) {
+            this.delegate = value;
+        }
+
+        public String xPath() {
+            String s = delegate.xPath();
+            if (s != null && s.startsWith("'") && s.endsWith("'"))
+                return s.toUpperCase();
+
+            return s;
+        }
     }
 
     @Override
